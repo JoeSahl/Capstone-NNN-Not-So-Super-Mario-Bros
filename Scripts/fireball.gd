6dbb28a8b2e96570192c5a4a2772915903b07e7e
ZacharyOOrtiz <zacharyortiz.5@gmail.com>
2024-11-14 15:46:01 -0500

    Updated Killzone and Fireball code
    Killzone now needs to account for big and fire Mario or else the game crashes. Also, I somehow screwed up my original way of using fireballs so I had to swap over to raycasts instead of relying a base collision layers. I'll see if it can be fixed later down the line.

--- Scripts/fireball.gd
+++ Scripts/fireball.gd
@@ -1,26 +1,32 @@
 extends Area2D
 
 var velocity = Vector2(500, 0)
-@onready var bounce_strength: float = -900.0
+#@onready var bounce_strength: float = -900.0
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var explosion_timer = $ExplosionTimer
+@onready var raycast = $RayCast2D
 
 func _ready() -> void:
 	add_to_group("Fireball")
 	animated_sprite.play("moving")
 
 func _process(delta) -> void:
-	self.velocity.y += gravity * delta
-	self.position += velocity * delta
+	velocity.y += gravity * delta
+	position += velocity * delta
+	
+	if raycast.is_colliding() and velocity.y > 0:
+		velocity.y = -abs(velocity.y)
 
 func _on_area_entered(area) -> void:
+#	if area.collision_layer == 1:
+#		print("tile hit")
+#		if velocity.y > 0:
+#			velocity.y = -abs(velocity.y)
 	if area.is_in_group("Enemy"):
 		area.die_to_fire()
-		self.velocity = Vector2.ZERO
+		velocity = Vector2.ZERO
 		animated_sprite.play("exploding")
 		explosion_timer.start()
-	elif area is StaticBody2D or area is CharacterBody2D or area is TileMapLayer:
-		velocity.y = bounce_strength
 
 func _on_explosion_timer_timeout() -> void:
 	queue_free()
--- Scripts/killzone.gd
+++ Scripts/killzone.gd
@@ -2,8 +2,12 @@ extends Area2D
 
 @onready var timer = $Timer
 
-func _on_body_entered(body: Node2D) -> void:
-	body.get_node("SmallCollisionShape2D").queue_free()
+func _on_body_entered(body) -> void:
+	if body.state == "small":
+		body.get_node("SmallCollisionShape2D").queue_free()
+	elif body.state == "big" or body.state == "fire": # NEEDS WORK
+		body.get_node("Big_FireCollisionShape2D").queue_free()
+	
 	timer.start()
 
 
